dd09c3ed309cf3a24fe9798d7631817d24ef44b4
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-10-19 16:54:07 +0300

    upd api auth

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
+
 import addQuestionsKindReducer from "./questions/questionsKind/questionsKind-reducer";
 
 const rootReducer = combineReducers({
