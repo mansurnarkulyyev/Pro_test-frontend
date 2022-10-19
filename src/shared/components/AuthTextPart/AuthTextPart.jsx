538752ba7cc3cc70218febf73cfb6d3720e2c7b6
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-10-19 11:36:38 +0300

    commit

--- src/shared/components/AuthTextPart/AuthTextPart.jsx
+++ src/shared/components/AuthTextPart/AuthTextPart.jsx
@@ -14,3 +14,4 @@ function AuthTextPart() {
 }
 
 export default AuthTextPart;
+
--- src/shared/components/DiagramRechart/DiagramRechart.jsx
+++ src/shared/components/DiagramRechart/DiagramRechart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import React, { useState, useEffect } from "react";
+
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 import Title from "../../components/Title/Title";
