3696c0dceae61cd9159469918bbf70857d6ae38e
B6X35 <d.b.palamarchuk@gmail.com>
2022-10-19 22:27:49 +0300

    Create NavAll.jsx

---
+++ src/modules/Header/NavAll/NavAll.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import NavPrivate from "./NavPrivate/NavPrivate";
+import NavPublic from "./NavPublic/NavPublic";
+import BurgerCloseButton from "...";
+import BurgerModal from "...";
+
+const NavAll = () => {
+    
+}
