9393bc48857b4e91b2ff81ce4e5e09c273a6d816
B6X35 <d.b.palamarchuk@gmail.com>
2022-10-20 16:19:42 +0300

    Update Logo.jsx

--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -1,2 +1,15 @@
-function Logo() {}
+import React from "react";
+import { Link } from 'react-router-dom'
+
+function Logo() {
+    return (
+        <>
+            <Link to="/">
+                <svg>
+                    <use></use>
+                </svg>
+            </Link>
+        </>
+    )
+}
 export default Logo;
