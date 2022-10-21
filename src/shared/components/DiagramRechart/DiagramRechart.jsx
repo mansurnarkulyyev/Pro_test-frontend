590bed41a9ef95b1fc245182a18bedbadb67677f
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-10-21 10:51:36 +0300

    create diagram file

--- src/shared/components/DiagramRechart/DiagramRechart.jsx
+++ src/shared/components/DiagramRechart/DiagramRechart.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
-
 import Title from "../../components/Title/Title";
 
 import photo from "../../../images/icons/010 1.png";
@@ -10,117 +7,31 @@ import Button from "../../components/Button/Button";
 import s from "./diagramRechart.module.scss";
 import classNames from "classnames";
 import { useNavigate } from "react-router-dom";
-
-
+import Diagram from "./Diagram/Diagram";
 
 function PieRechartComponent() {
 
-    // const [correct, setCorrect] = useState([]);
-    // const [incorrect, setIncorrect] = useState([]);
-
-    // useEffect(() => {
-    //     const getData = async () => {
-    // const correct = [];
-    // const incorrect = [];
-    //         const reqData = await fetch("https://");
-    //         const resData = await reqData.json();
-    // console.log(resData);
-    //         for (let i = 0; i < resData.length; i += 1) {
-    //             correct.push(resData[i].tech)
-    //             incorrect.push(resData[i].theory)
-
-    //         }
-
-    // setCorrect(correct);
-    // setIncorrect(incorrect);
-
-    //     }
-    //     getData();
-    // })
-
-
-    const COLORS = ["#FF6B01", "#D7D7D7"];
-    const pieData = [
-        {
-            name: "Correct",
-            value: 66.14
-        },
-        {
-            name: "Incorrect",
-            value: 10.35
-        }
-    ];
-    const CustomTooltip = ({ active, payload, label, value }) => {
-        if (active) {
-            return (
-                <div
-                    className="custom-tooltip"
-                    style={{
-                        backgroundColor: "#ffff",
-                        padding: "5px",
-                        border: "1px solid #cccc"
-                    }}
-                >
-                    <label>{`${payload[0].name} : ${payload[0].value}%`}</label>
-                    {/* <label>{`${[{ value: 'name', type: 'line', id: 'ID01' }]}`}</label> */}
-                </div>
-            );
-        }
-        return null;
-    };
-
     const navigate = useNavigate();
-
     const goToTest = () => {
-        navigate("/test");
+        navigate("/");
     };
-
 
     return (
         <div className={s.rechart}>
             <Title text="Results" />
 
             <span className={s.test_th}>[ TESTING THEORY_ ]</span>
-
             <div className={s.line}></div>
 
-            <PieChart width={500} height={300}>
-                <Pie
-                    data={pieData}
-                    color="#000000"
-                    dataKey="value"
-                    nameKey="name"
-                    cx="50%"
-                    cy="50%"
-                    stroke="none"
-                    outerRadius={120}
-                    fill="#FF6B01"
-                >
-                    {pieData.map((entry, index) => (
-                        <Cell
-                            key={`cell-${index}`}
-                            fill={COLORS[index % COLORS.length]}
-                        />
-                    ))}
-                </Pie>
-                <Tooltip content={<CustomTooltip />} />
-                <Legend layout="vertical" verticalAlign="middle" align="right" dataKey="value"
-                    nameKey="name" />
-                {/* <Legend content={<RenderLegend />} layout="vertical" verticalAlign="middle" align="right" /> */}
-
-            </PieChart>
+            <Diagram />
 
             <div className={s.result}>
                 <p>Correct answers - <strong>9</strong></p>
                 <div className={s.line2}>&#124;</div>
-
                 <p>Total questions - <strong>12</strong></p>
-
             </div>
-            <img
-                src={photo}
-                alt="01"
-            />
+
+            <img src={photo} alt="01" />
 
             <Title text="Not bad!" />
 
@@ -132,7 +43,6 @@ function PieRechartComponent() {
                 text="Try again"
                 onClick={goToTest}
             />
-
         </div>
     );
 }
--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import Logo from '../../../images/icons/logo.svg'
+import logo from '../../../images/icons/logo.svg'
 
 function Logo() {
     return (
         <>
             <Link to="/">
                 <svg width='129px' height='28px'>
-                    <use href={Logo}></use>
+                    <use href={logo}></use>
                 </svg>
             </Link>
         </>
