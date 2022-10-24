ebff36606b2a4cb941ec48c41ce12f0a2b53b24a
B6X35 <d.b.palamarchuk@gmail.com>
2022-10-24 18:39:21 +0300

    update

--- src/modules/Header/NavAll/NavPrivate/NavPrivate.jsx
+++ src/modules/Header/NavAll/NavPrivate/NavPrivate.jsx
@@ -1,27 +1,32 @@
 import React from "react";
-import s from "./privateNav.module.scss"
+import s from "./privateNav.module.scss";
 import { NavLink } from "react-router-dom";
 
 const NavPrivate = () => {
-    return (
-        <>
-            <li className={s.NavElem}>
-                <NavLink exact to="/" className={s.menu}>
-                    Home
-                </NavLink>
-            </li>
-            <li className={s.NavElem}>
-                <NavLink exact to="/useful-info" className={s.menu}>
-                    Materials
-                </NavLink>
-            </li>
-            <li className={s.NavElem}>
-                <NavLink exact to="/contacts" className={s.menu}>
-                    Contacts
-                </NavLink>
-            </li>
-        </>
-    );
+  function getClassName({ isActive }) {
+    const style = isActive ? s.isActive : s.menu;
+    return style;
+  }
+
+  return (
+    <>
+      <li className={s.NavElem}>
+        <NavLink exact to="/" className={getClassName}>
+          Home
+        </NavLink>
+      </li>
+      <li className={s.NavElem}>
+        <NavLink exact to="/useful-info" className={getClassName}>
+          Materials
+        </NavLink>
+      </li>
+      <li className={s.NavElem}>
+        <NavLink exact to="/contacts" className={getClassName}>
+          Contacts
+        </NavLink>
+      </li>
+    </>
+  );
 };
 
 export default NavPrivate;
