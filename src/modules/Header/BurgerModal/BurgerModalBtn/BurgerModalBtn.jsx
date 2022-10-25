84935f81c436896b2eda75882970a647527c7ae1
B6X35 <d.b.palamarchuk@gmail.com>
2022-10-25 18:27:04 +0300

    modal

--- src/modules/Header/BurgerModal/BurgerModalBtn/BurgerModalBtn.jsx
+++ src/modules/Header/BurgerModal/BurgerModalBtn/BurgerModalBtn.jsx
@@ -9,19 +9,22 @@ function BurgerModalBtn({ isActive, onClick }) {
     <>
       {!isActive ? (
         <>
+        <div className={s.burger}>
           <div className={s.avatar}>P</div>
           <div className={s.btnContainer}>
             <button className={s.btn} type="button" onClick={onClick}>
-              <svg width="16px" height="16px">
+              <svg width="20px" height="20px">
                 <use href={sprite + "#burger"}></use>
               </svg>
             </button>
           </div>
+        </div>
+          
         </>
       ) : (
         <div className={s.btnContainer}>
           <button className={s.btn} type="button" onClick={onClick}>
-            <svg width="16px" height="16px">
+            <svg width="26px" height="26px">
               <use href={sprite + "#close"}></use>
             </svg>
           </button>
