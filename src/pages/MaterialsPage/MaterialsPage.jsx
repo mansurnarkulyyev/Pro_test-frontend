8f0657425d280af7e6564e4227fc826b17828f6e
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-10-27 16:53:42 +0300

    mobile add content

--- src/pages/MaterialsPage/MaterialsPage.jsx
+++ src/pages/MaterialsPage/MaterialsPage.jsx
@@ -3,12 +3,22 @@ import s from "./materialsPage.module.scss";
 const MaterialsPage = () => {
   return (
     <main className={s.wrapper}>
-      <h1 className={s.title}>Useful literature</h1>
-      <ol className={s.literatureList}>
-        <li>Testing dot.com Savin.</li>
-        <li>A mental hospital in the hands of patients.</li>
-        <li>Scrum. J. Sutherland.</li>
-      </ol>
+      <div className={s.container}>
+        <h2 className={s.title}>Useful literature</h2>
+        <ol className={s.literatureList}>
+          <li>Testing dot.com Savin.</li>
+          <li>A mental hospital in the hands of patients.</li>
+          <li>Scrum. J. Sutherland.</li>
+        </ol>
+      </div>
+      <div className={s.container}>
+        <h2 className={s.title}>Useful resources</h2>
+        <ol className={s.literatureList}>
+          <li>Testing dot.com Savin.</li>
+          <li>A mental hospital in the hands of patients.</li>
+          <li>Scrum. J. Sutherland.</li>
+        </ol>
+      </div>
     </main>
   );
 };
