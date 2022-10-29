ceef463a11416b1d61f6fd56ef9d9c6e1f704efc
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-10-29 13:58:45 +0300

    Update Contacts.jsx

---
+++ src/modules/Contacts/Contacts.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import ContactForm from "./ContactsForm";
+import ContactList from "./ContactsList";
+
+import Modal from "../../shared/components/Modal";
+// import Spinner from "../../shared/components/Spinner";
+
+// import s from "./contacts.module.scss";
+
+import contactsList from "../../data/contactsList.json";
+
+function Contacts() {
+  const [openModal, setOpenModal] = useState(false);
+
+  function showModal() {
+    setOpenModal(true);
+  }
+  function closeModal() {
+    setOpenModal(false);
+  }
+  function onAdd(data) {
+    closeModal();
+  }
+
+  return (
+    <>
+      <ContactList items={contactsList} showModal={showModal} />
+      {openModal && (
+        <Modal closeModal={closeModal}>
+          <ContactForm onSubmit={onAdd} />
+        </Modal>
+      )}
+    </>
+  );
+}
+export default Contacts;
